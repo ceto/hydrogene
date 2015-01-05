a90314e23e67aa881806e601b2b06b33ff2ce4a8
Gabor Szabo <szabogabi@gmail.com>
2015-01-05 21:25:20 +0100

    izéke szöveg j

--- templates/aboutus-page.php
+++ templates/aboutus-page.php
@@ -15,6 +15,28 @@
 			<p class="whiteblock__disclaimer">A <strong>HYDROGENE</strong> egy két személyes design műhely. Kiemelkedő minőségű egyedi weboldalakat tervezünk hagyományos és mobil eszközökre. Van egy jó projekted? Itt vagyunk, segítünk.</p>
 	</div>
 </section>
+
+<section class="whiteblock">
+	<div class="wrapper wrapper--wide">
+			<h3 class="whiteblock__title"><small>a hydrogene</small>“alkotó” elemei</h3>
+			<p class="whiteblock__disclaimer">Ők jó szakemberek. Személyes kapcsolat, barátságosság. Találkozás. Bizalom. Alfa és omega, ők veszik fel a munkát és ők is végzik el. Ismerd meg őket </p>
+	</div>
+</section>
+
+<section class="whiteblock">
+	<div class="wrapper wrapper--wide">
+			<h3 class="whiteblock__title"><small>a hydrogene</small>“alkotó” elemei</h3>
+			<p class="whiteblock__disclaimer"> Szakemberként több éves tapasztalattal állunk  rendelkezésedre. Az ügyfeleinkkel személyes, szinte baráti és a tervezést illetően  bizalmi kapcsolatot tartunk fenn. A vállalt feladatot az első lépéstől az utolsóig mi végezzük el, mondhatni veled együtt tesszük meg, hisz bármikor informálunk a munka állásáról. Ismerj meg minket.  </p>
+	</div>
+</section>
+
+<section class="whiteblock">
+	<div class="wrapper wrapper--wide">
+			<h3 class="whiteblock__title"><small>akik</small>a Hydrogenet alkotják</h3>
+			<p class="whiteblock__disclaimer">A <strong>HYDROGENE</strong> egy két személyes design műhely. Kiemelkedő minőségű egyedi weboldalakat tervezünk hagyományos és mobil eszközökre. Van egy jó projekted? Itt vagyunk, segítünk.</p>
+	</div>
+</section>
+
 <div class="page__content">
 	<div class="wrapper wrapper--normal">
 		<?php the_content(); ?>
