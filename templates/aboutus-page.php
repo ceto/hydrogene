37f1e2a3098e654f620e5620059f4413291af237
Gabor Szabo <szabogabi@gmail.com>
2015-01-15 17:55:52 +0100

    aboutus savozas

--- templates/aboutus-page.php
+++ templates/aboutus-page.php
@@ -10,112 +10,145 @@
 	</div>
 </header>
 
-<section class="whiteblock">
+<section class="niceheading">
 	<div class="wrapper wrapper--wide">
-			<h3 class="whiteblock__title"><small>tervezési irányelveink</small>eszközök és módszerek</h3>
-			<p class="whiteblock__disclaimer">Grafikai arculatot és felhasználóbarát egyedi honlapokat tervezünk hagyományos és mobil eszközökre. Módszerünk átfogó, a tevékenységünk széleskörű.</p>
+			<h3 class="niceheading__title"><small>tervezési irányelveink</small>eszközök és módszerek</h3>
+			<p class="niceheading__disclaimer">Grafikai arculatot és felhasználóbarát egyedi honlapokat tervezünk hagyományos és mobil eszközökre. Módszerünk átfogó, a tevékenységünk széleskörű.</p>
 	</div>
 </section>
 
 <section class="process">
 					
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">
-				<header class="storyitem__head">	
-					<h5 class="storyitem__subtitle">Kutatás és felfedezés</h5>
-					<h4 class="storyitem__title">Elmélyülés a feladatban, biztos alapok rögzítése</h4>
-				</header>
-				
-				<div class="storyitem__desc">
-					<p>
-					A tervezés megkezdése előtt legfontosabb tájékozódni a feladat körülményeiről a környezetről. Kis esettanulmányt, kutatást végzünk, megvizsgáljuk a konkurensek piaci szereplését, megjelenését. Elmélyülünk a tevékenységben, legyen az turisztikai vagy akár egészségügyi szakterületen.</p>
-					<p>Hiszünk abban, hogy csak a megfelelő tájékozottság, tisztánlátás melett lehet elkezdeni a megalapozott tervezési munkát</p>
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-crosshair"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">	
+						<h5 class="widestoryitem__subtitle">Kutatás és felfedezés</h5>
+						<h4 class="widestoryitem__title">Elmélyülés a feladatban, biztos alapok rögzítése</h4>
+					</header>
+					<div class="widestoryitem__desc">
+						<p>
+						A tervezés megkezdése előtt legfontosabb tájékozódni a feladat körülményeiről a környezetről. Kis esettanulmányt, kutatást végzünk, megvizsgáljuk a konkurensek piaci szereplését, megjelenését. Elmélyülünk a tevékenységben, legyen az turisztikai vagy akár egészségügyi szakterületen.</p>
+						<p>Hiszünk abban, hogy csak a megfelelő tájékozottság, tisztánlátás melett lehet elkezdeni a megalapozott tervezési munkát</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">
-				<header class="storyitem__head">	
-					<h5 class="storyitem__subtitle">Arculat és Design</h5>					
-					<h4 class="storyitem__title">Karakteres arculat kialakítása és következetes használata</h4>
-				</header>
-				<div class="storyitem__desc">
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-skullandcrossbones"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">	
+						<h5 class="widestoryitem__subtitle">Arculat és Design</h5>					
+						<h4 class="widestoryitem__title">Karakteres arculat kialakítása és következetes használata</h4>
+					</header>
+					<div class="widestoryitem__desc">
 
-					<p>
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse	cillum dolore eu fugiat nulla pariatur.</p>
-					<p>Lorem ipsum marika ámen. A tejfölös doboz dolor in reprehenderit in vo.</p>
+						<p>
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse	cillum dolore eu fugiat nulla pariatur.</p>
+						<p>Lorem ipsum marika ámen. A tejfölös doboz dolor in reprehenderit in vo.</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">	
-				<header class="storyitem__head">
-					<h5 class="storyitem__subtitle">Reszponzív honlap</h5>
-					<h4 class="storyitem__title">Weboldalad minden eszközön élményt nyújt</h4>
-				</header>
-				<div class="storyitem__desc">
-					<p>Honlapod rugalmasan illeszkedik a végtelen számú eszköztípus kijelzőjére. Kompromisszum mentes élményt nyújt mobilon, tableten és asztali számítógépen is.</p>
-					<p>Nem csupán formai-, hanem tartalom szervezési metodika a <em>“Mobil az első”</em> tervezési módszer. Hierarchizáljuk a tartalmat. A minimális és elégséges információ tálalásával kezdünk, majd bővítjük azt és haladunk a “nagyképernyős” egész felé.</p>
-					
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-mug"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">
+						<h5 class="widestoryitem__subtitle">Reszponzív honlap</h5>
+						<h4 class="widestoryitem__title">Weboldalad minden eszközön élményt nyújt</h4>
+					</header>
+					<div class="widestoryitem__desc">
+						<p>Honlapod rugalmasan illeszkedik a végtelen számú eszköztípus kijelzőjére. Kompromisszum mentes élményt nyújt mobilon, tableten és asztali számítógépen is.</p>
+						<p>Nem csupán formai-, hanem tartalom szervezési metodika a <em>“Mobil az első”</em> tervezési módszer. Hierarchizáljuk a tartalmat. A minimális és elégséges információ tálalásával kezdünk, majd bővítjük azt és haladunk a “nagyképernyős” egész felé.</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">	
-				<header class="storyitem__head">
-					<h5 class="storyitem__subtitle">Legújabb technológia</h5>
-					<h4 class="storyitem__title">Jövőbarát korszerű fejlesztés és naprakész tudás</h4>
-				</header>
-				<div class="storyitem__desc">
-					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p>Axcepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sint amet Ernőbácsi orem ipsum marika ámen</p>
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-helm"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">
+						<h5 class="widestoryitem__subtitle">Legújabb technológia</h5>
+						<h4 class="widestoryitem__title">Jövőbarát korszerű fejlesztés és naprakész tudás</h4>
+					</header>
+					<div class="widestoryitem__desc">
+						<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Axcepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sint amet Ernőbácsi orem ipsum marika ámen</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">
-				<header class="storyitem__head">	
-					<h5 class="storyitem__subtitle">Tartalomkezelés</h5>
-					<h4 class="storyitem__title">Honlapod tartalma házon belül szerkeszthető</h4>
-				</header>
-				<div class="storyitem__desc">
-					<p>Elsősorban a legnépszerűbb nyílt forrású tartalomkezelőre Wordpressre, fejlesztünk sablonmentes, teljesen egyedi megjelenésű és testreszabott funkciókkal bíró honlapokat. A tartalmak, jól ismert, könnyen kezelhető és intuitiv adminisztrációs felületen szerkeszthetők.</p>
-					<p>Az elkészített honlap teljes egészében a megrendelő tulajdonába kerül. Nincsenek további rejtett havi díjak és üzembentartási költségek.</p>
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-golf"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">	
+						<h5 class="widestoryitem__subtitle">Tartalomkezelés</h5>
+						<h4 class="widestoryitem__title">Honlapod tartalma házon belül szerkeszthető</h4>
+					</header>
+					<div class="widestoryitem__desc">
+						<p>Elsősorban a legnépszerűbb nyílt forrású tartalomkezelőre Wordpressre, fejlesztünk sablonmentes, teljesen egyedi megjelenésű és testreszabott funkciókkal bíró honlapokat. A tartalmak, jól ismert, könnyen kezelhető és intuitiv adminisztrációs felületen szerkeszthetők.</p>
+						<p>Az elkészített honlap teljes egészében a megrendelő tulajdonába kerül. Nincsenek további rejtett havi díjak és üzembentartási költségek.</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">
-				<header class="storyitem__head">	
-					<h5 class="storyitem__subtitle">Stratégia és Marketing</h5>
-					<h4 class="storyitem__title">Tudatosan a sikerért és az ügyfeleidért</h4>
-				</header>
-				<div class="storyitem__desc">
-					<p>
-				Porem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p>Terézia zongorázik vadul. Nálam excepteur sint occaecat cupidatat non proident. Lorem ipsum marika ámen</p>
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-fuel"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">	
+						<h5 class="widestoryitem__subtitle">Stratégia és Marketing</h5>
+						<h4 class="widestoryitem__title">Tudatosan a sikerért és az ügyfeleidért</h4>
+					</header>
+					<div class="widestoryitem__desc">
+						<p>
+					Porem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Terézia zongorázik vadul. Nálam excepteur sint occaecat cupidatat non proident. Lorem ipsum marika ámen</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="storyitem">
+		<div class="widestoryitem">
 			<div class="wrapper wrapper--wide">
-				<header class="storyitem__head">	
-					<h5 class="storyitem__subtitle">Személyes kapcsolat</h5>					
-					<h4 class="storyitem__title">Közvetlenül a tervezővel állsz kapcsolatban</h4>
-				</header>
-				<div class="storyitem__desc">
+				<figure class="widestoryitem__figure">
+					<i class="ss-icon ss-thunderstorm"></i>
+				</figure>
+				<div class="widestoryitem__textcontent">
+					<header class="widestoryitem__head">	
+						<h5 class="widestoryitem__subtitle">Személyes kapcsolat</h5>					
+						<h4 class="widestoryitem__title">Közvetlenül a tervezővel állsz kapcsolatban</h4>
+					</header>
+					<div class="widestoryitem__desc">
 
-					<p>
-					Gördülékeny ügymenet. Minden egy kézben. Piciny design csapatunk éllovasai szenvedélyes digitális kézművesek akik, &mdash; a tervezésen és kivitelezésen túl &mdash; döntéseket hoznak és kommunikálnak veled. </p>
-					<p>Nagy ügynökségekkel ellentétben, &mdash; az effektív munkát hátráltató &mdash; ügymeneti körök nélkül közvetlenül a tervezővel állsz kapcsolatban.</p>
+						<p>
+						Gördülékeny ügymenet. Minden egy kézben. Piciny design csapatunk éllovasai szenvedélyes digitális kézművesek akik, &mdash; a tervezésen és kivitelezésen túl &mdash; döntéseket hoznak és kommunikálnak veled. </p>
+						<p>Nagy ügynökségekkel ellentétben, &mdash; az effektív munkát hátráltató &mdash; ügymeneti körök nélkül közvetlenül a tervezővel állsz kapcsolatban.</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -124,10 +157,10 @@
 
 
 
-<section class="whiteblock">
+<section class="niceheading">
 	<div class="wrapper wrapper--wide">
-			<h3 class="whiteblock__title"><small>a hydrogene</small>“alkotó” elemei</h3>
-			<p class="whiteblock__disclaimer"><strong>Ismerj meg minket</strong> Piciny design csapatunk éllovasai szenvedélyes digitális kézművesek akik, &mdash; a tervezésen és kivitelezésen túl &mdash; döntéseket hoznak és kommunikálnak veled.</p>
+			<h3 class="niceheading__title"><small>Kik vagyunk</small>hydrogene alkotó elemei</h3>
+			<p class="niceheading__disclaimer"><strong>Ismerj meg minket</strong> Piciny design csapatunk éllovasai szenvedélyes digitális kézművesek akik, &mdash; a tervezésen és kivitelezésen túl &mdash; döntéseket hoznak és kommunikálnak veled.</p>
 	</div>
 </section>
 
@@ -219,50 +252,56 @@
 </section>
 
 
-<section class="darkblock">
+<section class="niceheading niceheading--inverse">
 	<div class="wrapper wrapper--wide">
-			<h3 class="darkblock__title"><small>értjük a dolgunkat</small>Szolgáltatásaink</h3>
-			<p class="darkblock__disclaimer">Grafikai arculatot és felhasználóbarát egyedi honlapokat tervezünk hagyományos és mobil eszközökre. Módszerünk átfogó, a tevékenységünk széleskörű.</p>
-			<section class="services">
-				<div class="wrapper wrapper--wide">
-					<div class="servicegroup">
-						<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_arculat.svg';?>" alt="Arculat" class="servicegroup__ill">
-						<h4 class="servicegroup__name">Arculat</h4>
-						<ul class="servicelist">
-							<li>Arculattervezés</li>
-							<li>Logó tervezés</li>
-							<li>Csomagolás tervezés</li>
-							<li>Plakát és brossúra</li>
-							<li>Belsőépítészet</li>
-						</ul>
-					</div>
-					<div class="servicegroup">
-						<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_mobilweb.svg';?>" alt="Web/mobil" class="servicegroup__ill">
-						<h4 class="servicegroup__name">Web/mobil</h4>
-						<ul class="servicelist">
-							<li>Webdesign</li>
-							<li>UX tervezés</li>
-							<li>Mobilbarát honlap</li>
-							<li>Tartalom kezelés</li>
-							<li>Mikroszájtok</li>
-							<li>Kampányoldal</li>
-						</ul>
-					</div>
-					<div class="servicegroup">
-						<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_strategia.svg';?>" alt="Stratégia" class="servicegroup__ill">
-						<h4 class="servicegroup__name">Stratégia</h4>
-						<ul class="servicelist">
-							<li>Online marketing</li>
-							<li>Keresőoptimalizálás</li>
-							<li>Analitika</li>
-							<li>Tartalom stratégia</li>
-							<li>Hírlevél kampány</li>
-						</ul>
-					</div>
-				</div>				
-			</section>
+			<h3 class="niceheading__title"><small>értjük a dolgunkat</small>Szolgáltatásaink</h3>
+			<p class="niceheading__disclaimer">Grafikai arculatot és felhasználóbarát egyedi honlapokat tervezünk hagyományos és mobil eszközökre. Módszerünk átfogó, a tevékenységünk széleskörű.</p>
 	</div>
 </section>
+
+<section class="services">
+	<div class="wrapper wrapper--wide">
+		<div class="servicegroup">
+			<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_arculat.svg';?>" alt="Arculat" class="servicegroup__ill">
+			<i class="ss-icon ss-mug"></i>
+			<h4 class="servicegroup__name">Arculat</h4>
+			<ul class="servicelist">
+				<li>Arculattervezés</li>
+				<li>Logó tervezés</li>
+				<li>Csomagolás tervezés</li>
+				<li>Plakát és brossúra</li>
+				<li>Belsőépítészet</li>
+			</ul>
+		</div>
+		<div class="servicegroup">
+			<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_mobilweb.svg';?>" alt="Web/mobil" class="servicegroup__ill">
+			<i class="ss-icon ss-golf"></i>
+			<h4 class="servicegroup__name">Web/mobil</h4>
+			<ul class="servicelist">
+				<li>Webdesign</li>
+				<li>UX tervezés</li>
+				<li>Mobilbarát honlap</li>
+				<li>Tartalom kezelés</li>
+				<li>Mikroszájtok</li>
+				<li>Kampányoldal</li>
+			</ul>
+		</div>
+		<div class="servicegroup">
+			<img src="<?php echo get_stylesheet_directory_uri().'/assets/img/ikon_strategia.svg';?>" alt="Stratégia" class="servicegroup__ill">
+			<i class="ss-icon ss-crosshair"></i>
+			<h4 class="servicegroup__name">Stratégia</h4>
+			<ul class="servicelist">
+				<li>Online marketing</li>
+				<li>Keresőoptimalizálás</li>
+				<li>Analitika</li>
+				<li>Tartalom stratégia</li>
+				<li>Hírlevél kampány</li>
+			</ul>
+		</div>
+	</div>				
+</section>
+
+
 
 
 <!-- <div class="page__content">
