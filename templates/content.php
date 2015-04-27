4110b90a95242becd5114119ec40b49227051ea6
Gabor Szabo <szabogabi@gmail.com>
2015-04-27 14:15:41 +0200

    some chars [deploy: Live]

--- templates/content.php
+++ templates/content.php
@@ -2,24 +2,24 @@
   <?php get_template_part('templates/content', 'sticky'); ?>
 <?php else : ?>
 <article <?php post_class('archiveentry animated fadeInUp'); ?>>
-    <header class="archiveentry__header niceheading">
-      <div class="wrapper wrapper--normal">
+  <header class="archiveentry__header niceheading">
+    <div class="wrapper wrapper--normal">
 
-        <h2 class="archiveentry__title niceheading__title">
-        	<a href="<?php the_permalink(); ?>">
-        		<?php the_title(); ?>
-        	</a>
-        </h2>
-        <section class="niceheading__preinfo archiveentry__meta">
-          <?php get_template_part('templates/entry-meta'); ?>          
-        </section>
+      <h2 class="archiveentry__title niceheading__title">
+      	<a href="<?php the_permalink(); ?>">
+      		<?php the_title(); ?>
+      	</a>
+      </h2>
+      <section class="niceheading__preinfo archiveentry__meta">
+        <?php get_template_part('templates/entry-meta'); ?>          
+      </section>
 
-        <p class="archiveentry__summary niceheading__disclaimer">
-          <?php echo get_the_excerpt(); ?>
-        </p>
+      <p class="archiveentry__summary niceheading__disclaimer">
+        <?php echo get_the_excerpt(); ?>
+      </p>
 
-        <a class="readmore" href="<?php the_permalink();?>"><?php _e('Tovább olvasom…','h2') ?></a>
-      </div>
-    </header>
+      <a class="readmore" href="<?php the_permalink();?>"><?php _e('Tovább olvasom…','h2') ?></a>
+    </div>
+  </header>
 </article>
 <?php endif; ?>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,15 +1,14 @@
 <div class="wrapper wrapper--wide">
   <header class="refarchive__header niceheading niceheading--ashero">
-
-      <section class="niceheading__preinfo animated fadeInUp">
-        <span class="feat"><?php echo roots_subtitle(); ?></span>          
-      </section>
-      <h1 class="singlepost__title niceheading__title animated fadeInUp">
-        <?php echo roots_title(); ?>
-      </h1>
-      <p class="niceheading__disclaimer animated fadeInUp">
-        <?php echo roots_description(); ?>
-      </p>
+    <section class="niceheading__preinfo animated fadeInUp">
+      <span class="feat"><?php echo roots_subtitle(); ?></span>          
+    </section>
+    <h1 class="singlepost__title niceheading__title animated fadeInUp">
+      <?php echo roots_title(); ?>
+    </h1>
+    <p class="niceheading__disclaimer animated fadeInUp">
+      <?php echo roots_description(); ?>
+    </p>
   </header>
 </div>
 
